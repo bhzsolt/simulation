3a8ebf56fc9383e9938f270be3fa59c8f10b43ce
Bodoki-Halmen Zsolt <bokodihalmen@yahoo.com>
2020-10-23 15:27:05 +0300

    updated tests

--- src/test/color_test.c
+++ src/test/color_test.c
@@ -9,5 +9,6 @@ int main(void) {
 	printf("Size of force is %ld\n", sizeof(particle.force));
 	printf("Size of direction is %ld\n", sizeof(particle.direction));
 	printf("Size of color is %ld\n", sizeof(particle.color));
+	printf("Size of const char * is %ld\n", sizeof(const char *));
 	exit(EXIT_SUCCESS);
 }
